5e51aa85c5e95b58baf781ab3ba64acb6ee21e4c
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2022-11-05 20:48:29 +0700

    try gitignore

---
+++ prepareDataTraining.py
@@ -0,0 +1,94 @@
+import numpy as np
+import os
+import cv2
+import mediapipe as mp
+from keyPointMP import mediapipe_detection, draw_styled_landmarks
+
+mp_holistic = mp.solutions.holistic  # Holistic model
+mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join('MP_Data')
+
+# Actions that we try to detect
+actions = np.array(['hello', 'thanks', 'iloveyou'])
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+
+def create_folder():
+    for action in actions:
+        for sequence in range(no_sequences):
+            try:
+                os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+            except Exception:
+                pass
+
+
+def extract_keypoints(results):
+    if results.pose_landmarks:
+        pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten()
+    else:
+        pose = np.zeros(33*4)
+
+    if results.face_landmarks:
+        face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten()
+    else:
+        face = np.zeros(468*3)
+
+    if results.left_hand_landmarks:
+        lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten()
+    else:
+        lh = np.zeros(21*3)
+
+    if results.right_hand_landmarks:
+        rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten()
+    else:
+        rh = np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
+
+
+def take_keypoints_from_video():
+    cap = cv2.VideoCapture(0)
+    # Set mediapipe model
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+
+        # NEW LOOP
+        # Loop through actions
+        for action in actions:
+            # Loop through sequences aka videos
+            for sequence in range(no_sequences):
+                # Loop through video length aka sequence length
+                for frame_num in range(sequence_length):
+
+                    # Read feed
+                    ret, frame = cap.read()
+
+                    # Make detections
+                    image, results = mediapipe_detection(frame, holistic)
+                    # print(results)
+
+                    # Draw landmarks
+                    draw_styled_landmarks(image, results)
+
+                    # NEW Apply wait logic
+                    if frame_num == 0:
+                        cv2.putText(image, 'STARTING COLLECTION', (120, 200),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
+                        cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence),
+                                    (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                        # Show to screen
+                        cv2.imshow('OpenCV Feed', image)
+                        cv2.waitKey(2000)
+
+                    else:
+                        cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence),
+                                    (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                        # Show to screen
+                        cv2.imshow('OpenCV Feed', image)
+
+
+take_keypoints_from_video()
