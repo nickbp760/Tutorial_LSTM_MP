3b7d19538fab1a7d8f34eded0dddfa1d52bb1350
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2023-03-15 20:17:33 +0700

    pusing

--- Face_Distraction/RealTimeTest_FaceCompleteIris_GRU.py
+++ Face_Distraction/RealTimeTest_FaceCompleteIris_GRU.py
@@ -33,7 +33,7 @@ def model_reload():
     model.add(Dense(32, activation='relu'))
     model.add(Dense(actions.shape[0], activation='softmax'))
 
-    model.load_weights('Weight_model/actionGRUFaceCompleteIris.h5')
+    model.load_weights('Weight_model/actionGRUFaceNikoComplete.h5')
     return model
 
 
