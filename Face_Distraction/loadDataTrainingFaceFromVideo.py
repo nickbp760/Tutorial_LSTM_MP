982ac65de8acd65424a416a0754bdc9abe669d64
nickbp760 <56335338+nickbp760@users.noreply.github.com>
2023-07-02 15:26:10 +0700

    ValidCheatNoNorm

--- Face_Distraction/loadDataTrainingFaceFromVideo.py
+++ Face_Distraction/loadDataTrainingFaceFromVideo.py
@@ -57,8 +57,8 @@ def take_keypoints_Completeface_from_video(DATA_PATH: str, action: str, sequnce:
 
 
 # specify the directory path
-path = "./FaceVideoDataset/FaceTestVideoDataset"
-DATA_PATH = os.path.join('./Real_CheatDataNoNorm/Real_CheatDataNoNormTest')
+path = "./FaceVideoDataset/FaceTrainVideoDataset"
+DATA_PATH = os.path.join('./Real_CheatData/Real_CheatDataTrain')
 # use os.listdir() to get a list of all files and folders in the directory
 filesAndFolders = os.listdir(path)
 
